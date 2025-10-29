d1a165ce286069f934cc19c5199dd2b31fdb48d2
alfredodasser <alfredo.benaute@dasser.com.pe>
2025-10-28 21:45:12 -0500

    feat: add data sql

--- database/data.sql
+++ database/data.sql
@@ -2,8 +2,8 @@ create table usuario(
     id int,
     nombre varchar(250),
     email varchar(250),
-    direccion varchar(500) null
+    direcciones varchar(500) null
 );
 
 select id, getdate() as Fecha
-from usuario:
+from usuario;
